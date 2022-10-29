d752f274e8973b5dd756dd4ebe68715716958c61
Ismael Yahyaoui Racine <iyahoui-@c2r2p3.42quebec.com>
2022-10-29 17:45:14 -0400

    Started working on specular reflection

--- srcs/render/light.c
+++ srcs/render/light.c
@@ -4,7 +4,6 @@ t_color	apply_point_lights(t_data *rt, t_hit_rec *rec, t_color color)
 {
 	t_ray_vec3	pt_to_light;
 	t_hit_rec	rec2;
-	t_vec3		diff;
 	double		dist_to_light;
 	double		t;
 	int			i;
@@ -12,20 +11,29 @@ t_color	apply_point_lights(t_data *rt, t_hit_rec *rec, t_color color)
 	i = -1;
 	while (++i < rt->nb_lights)
 	{
+		// Diffuse lighting
 		pt_to_light.orig = rec->p;
-		diff = sub_vec3(rt->lights[i]->pos, rec->p);
-		pt_to_light.dir = diff;
+		pt_to_light.dir = sub_vec3(rt->lights[i]->pos, rec->p);
+
+		// Small optimization to fail check early
+		if (dot_vec3(pt_to_light.dir, rec->normal) <= 0.0F)
+			continue ;
 
 		// To verify objects hits are happening before light hit
-		dist_to_light = length_vec3(diff);
+		dist_to_light = length_vec3(pt_to_light.dir);
 		rec2.t = dist_to_light;
 		unit_vec3_self(&pt_to_light.dir);
 		// Test for hard shadows, correct to prevent shadow acne
 		if (hit_anything(rt, &pt_to_light, &rec2) && fabs(rec2.t - dist_to_light) > EPSILON)
-			continue;
-		t = cos_vec3(rec->normal, diff) * LIGHT_INTENSITY;
+			continue ;
+		// t = cos_vec3(rec->normal, pt_to_light.dir);
+		t = cos_vec3(rec->normal, pt_to_light.dir) * LIGHT_INTENSITY;
 		t /= (dist_to_light + 1.0F);
 		color = lerp_color(color, rt->lights[i]->clr, t);
+
+
+		// Specular reflection
+
 	}
 	return (color);
 }
