2d90511c54b24374357fa1822b45d266a87584e8
Ismael Yahyaoui Racine <iyahoui-@c3r11p2.42quebec.com>
2022-11-15 14:35:45 -0500

    Split apply_textures() into a function call

--- srcs/render/render.c
+++ srcs/render/render.c
@@ -6,7 +6,7 @@
 /*   By: iyahoui- <iyahoui-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 14:18:56 by iyahoui-          #+#    #+#             */
-/*   Updated: 2022/11/15 14:21:02 by iyahoui-         ###   ########.fr       */
+/*   Updated: 2022/11/15 14:35:42 by iyahoui-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,30 +28,41 @@ bool	hit_anything(t_data *rt, t_ray *r, t_hit_rec *rec)
 	return (rec->hit_anything);
 }
 
+static void	apply_textures(t_data *rt, t_hit_rec *rec)
+{
+	t_vec3	p;
+	t_mat4	rot;
+
+	if ((rt->toggle.is_texture || rt->toggle.is_normal)
+		&& rec->obj->type == T_SPH)
+	{
+		p = sub_vec3(rec->p, rec->obj->pos);
+		rot = mat_rot_compound(
+				deg_to_rad(rec->obj->rot.x),
+				deg_to_rad(rec->obj->rot.y),
+				deg_to_rad(rec->obj->rot.z));
+		p = vec4_to_vec3(mat_mult_vec4(vec3_to_vec4(p, T_POINT), rot));
+		if ((rec->obj->texture.is_image || rec->obj->texture.is_checkers)
+			&& rt->toggle.is_texture)
+			rec->color = obj_get_color(rt, p, rec->obj);
+		if (rt->toggle.is_normal && rec->obj->normal.is_image)
+			rec->normal = obj_get_normal(rec->normal, p, rec->obj);
+	}
+}
+
 /* Get final colour values for a single pixel */
 static inline t_color	render_pixel(t_data *rt, t_ray *r, t_hit_rec *rec)
 {
 	register t_color	pixel_color;
-	t_vec3				p;
-	t_mat4				rot;
 
 	rec->t = T_MAX;
 	rec->hit_anything = false;
 	if (hit_anything(rt, r, rec))
 	{
-		if ((rt->toggle.is_texture || rt->toggle.is_normal) && rec->obj->type == T_SPH)
-		{
-			p = sub_vec3(rec->p, rec->obj->pos);
-			rot = mat_rot_compound(deg_to_rad(rec->obj->rot.x), deg_to_rad(rec->obj->rot.y), deg_to_rad(rec->obj->rot.z));
-			p = vec4_to_vec3(mat_mult_vec4(vec3_to_vec4(p, T_POINT), rot));
-			if (rt->toggle.is_texture && (rec->obj->texture.is_image || rec->obj->texture.is_checkers))
-				rec->color = obj_get_color(rt, p, rec->obj);
-			if (rt->toggle.is_normal && rec->obj->normal.is_image)
-				rec->normal = obj_get_normal(rec->normal, p, rec->obj);
-		}
+		apply_textures(rt, rec);
 		pixel_color = pixel_shader(rt, rec, rec->color);
 	}
-	else 
+	else
 		pixel_color = rt->background;
 	if (rt->toggle.is_light_halo)
 		pixel_color = apply_light_halos(rt, r, rec, pixel_color);
